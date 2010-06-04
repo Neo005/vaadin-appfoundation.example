15b1e8f0d5f1cad6314d41777756b9fde2246091
kim <kim@localhost>
2010-06-04 13:51:52 +0300

    Parse the text to HTML before rendering

--- src/org/vaadin/appfoundation/example/authentication/AuthIntro.java
+++ src/org/vaadin/appfoundation/example/authentication/AuthIntro.java
@@ -3,29 +3,34 @@ package org.vaadin.appfoundation.example.authentication;
 import org.vaadin.appfoundation.i18n.Lang;
 import org.vaadin.appfoundation.view.AbstractView;
 
+import ys.wikiparser.WikiParser;
+
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 
 public class AuthIntro extends AbstractView<VerticalLayout> {
 
-    private static final long serialVersionUID = 3319859864849238330L;
+	private static final long serialVersionUID = 3319859864849238330L;
 
-    public AuthIntro() {
-        super(new VerticalLayout());
-        getContent().addComponent(
-                new Label(Lang.getMessage("intro text to auth module")));
-    }
+	public AuthIntro() {
+		super(new VerticalLayout());
 
-    @Override
-    public void activated(Object... params) {
-        // TODO Auto-generated method stub
+		getContent().addComponent(
+				new Label(WikiParser.renderXHTML(Lang
+						.getMessage("intro text to auth module")),
+						Label.CONTENT_XHTML));
+	}
 
-    }
+	@Override
+	public void activated(Object... params) {
+		// TODO Auto-generated method stub
 
-    @Override
-    public void deactivated(Object... params) {
-        // TODO Auto-generated method stub
+	}
 
-    }
+	@Override
+	public void deactivated(Object... params) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
